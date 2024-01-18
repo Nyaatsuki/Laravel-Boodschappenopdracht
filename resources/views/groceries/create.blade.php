34b2a68b74e46078b90c3ac4c387fc6b3fea3a66
Nyaatsuki <puttinpanfu@gmail.com>
2024-01-18 11:17:39 +0100

    Create Page styling

--- resources/views/groceries/create.blade.php
+++ resources/views/groceries/create.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
-    <img src="https://media.giphy.com/media/k5dBf28AbPVUFDosHS/giphy.gif">
+    <div class="create-page">
+        <label>Product:</label>
+        <input style="margin-left: 2px;" type="product"><br><br>
+        <label>Price:</label>
+        <input style="margin-left: 24px;" type="price"><br><br>
+        <label>Amount:</label>
+        <input type="amount"><br><br>
+        <label>Total:</label>
+        <input style="margin-left: 24px;" type="total"><br><br>
+    </div>
 </x-layout>
