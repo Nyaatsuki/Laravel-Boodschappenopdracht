6ecd294c89be7dd5b444567466a6563226e28036
Nyaatsuki <puttinpanfu@gmail.com>
2024-01-23 12:08:40 +0100

    Styling

--- resources/views/groceries/create.blade.php
+++ resources/views/groceries/create.blade.php
@@ -1,11 +1,13 @@
 <x-layout>
     <div class="create-page">
-        <label>Product:</label>
-        <input style="margin-left: 2px;" type="product"><br><br>
-        <label>Price:</label>
-        <input style="margin-left: 24px;" type="price"><br><br>
-        <label>Amount:</label>
-        <input type="amount"><br><br>
+        <div class="create-form">
+            <label>Product:</label>
+            <input style="margin-left: 2px;" type="product"><br><br>
+            <label>Price:</label>
+            <input style="margin-left: 24px;" type="price"><br><br>
+            <label>Amount:</label>
+            <input type="amount"><br>
+        </div>
         <button class=create-button type="button">Create</button>
     </div>
 </x-layout>
--- resources/views/groceries/index.blade.php
+++ resources/views/groceries/index.blade.php
@@ -15,7 +15,7 @@
         <!--Bottom Row-->
         <div class="product" id="bottom-row"></div>
         <div class="prijs" id="bottom-row"></div>
-        <div class="aantal" id="bottom-row">Totaal</div>
+        <div class="aantal" id="bottom-row">Totaal:</div>
         <div class="subtotaal" id="bottom-row">€{{ array_sum($total) }}</disv>
     </section>
 </x-layout>
