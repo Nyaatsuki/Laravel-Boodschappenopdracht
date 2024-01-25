38b350c8fe0fce3d6e22852504304d969a9f7fbc
Nyaatsuki <puttinpanfu@gmail.com>
2024-01-25 12:18:03 +0100

    Redo stylesheet

--- resources/views/groceries/index.blade.php
+++ resources/views/groceries/index.blade.php
@@ -1,21 +1,23 @@
 <x-layout>
-    <section class="tabel">
-        <!--Top Row-->
-        <div class="product" id="top-row">Product</div>
-        <div class="prijs" id="top-row">Amount</div>
-        <div class="aantal" id="top-row">Price</div>
-        <div class="subtotaal" id="top-row">subtotal</div>
-        <!--Entries TODO: Cleanup display in inspector-->
-        @foreach ($groceries as $grocery)
-        <li>{{ $grocery->name }}</li>
-        <li>{{ $grocery->amount}}</li>
-        <li>€{{ number_format($grocery->price, 2)}}</li>
-        <li>€{{ number_format($grocery->price * $grocery->amount, 2)}}</li>
-        @endforeach
-        <!--Bottom Row-->
-        <div class="product" id="bottom-row"></div>
-        <div class="prijs" id="bottom-row"></div>
-        <div class="aantal" id="bottom-row">Total:</div>
-        <div class="subtotaal" id="bottom-row">€{{ number_format(array_sum($total), 2) }}</disv>
+    <section class="index-page">
+        <section class="tabel">
+            <!--Top Row-->
+            <div class="product" id="top-row">Product</div>
+            <div class="prijs" id="top-row">Amount</div>
+            <div class="aantal" id="top-row">Price</div>
+            <div class="subtotaal" id="top-row">subtotal</div>
+            <!--Entries TODO: Cleanup display in inspector-->
+            @foreach ($groceries as $grocery)
+            <li>{{ $grocery->name }}</li>
+            <li>{{ $grocery->amount}}</li>
+            <li>€{{ number_format($grocery->price, 2)}}</li>
+            <li>€{{ number_format($grocery->price * $grocery->amount, 2)}}</li>
+            @endforeach
+            <!--Bottom Row-->
+            <div class="product" id="bottom-row"></div>
+            <div class="prijs" id="bottom-row"></div>
+            <div class="aantal" id="bottom-row">Total:</div>
+            <div class="subtotaal" id="bottom-row">€{{ number_format(array_sum($total), 2) }}</disv>
+        </section>
     </section>
 </x-layout>
