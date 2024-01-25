4c1e4035b143614d06cb0971031d250549c3a1e8
Nyaatsuki <puttinpanfu@gmail.com>
2024-01-25 10:52:37 +0100

    English translation

--- resources/views/groceries/index.blade.php
+++ resources/views/groceries/index.blade.php
@@ -2,9 +2,9 @@
     <section class="tabel">
         <!--Top Row-->
         <div class="product" id="top-row">Product</div>
-        <div class="prijs" id="top-row">Aantal</div>
-        <div class="aantal" id="top-row">prijs</div>
-        <div class="subtotaal" id="top-row">Subtotaal</div>
+        <div class="prijs" id="top-row">Amount</div>
+        <div class="aantal" id="top-row">Price</div>
+        <div class="subtotaal" id="top-row">subtotal</div>
         @foreach ($groceries as $grocery)
         <!--Entries-->
         <li>{{ $grocery->name }}</li>
@@ -15,7 +15,7 @@
         <!--Bottom Row-->
         <div class="product" id="bottom-row"></div>
         <div class="prijs" id="bottom-row"></div>
-        <div class="aantal" id="bottom-row">Totaal:</div>
+        <div class="aantal" id="bottom-row">Total:</div>
         <div class="subtotaal" id="bottom-row">€{{ array_sum($total) }}</disv>
     </section>
 </x-layout>
