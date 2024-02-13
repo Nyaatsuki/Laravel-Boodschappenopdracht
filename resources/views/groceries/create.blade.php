dff070af902503167db6c59445af7cccfce3c85a
Nyaatsuki <puttinpanfu@gmail.com>
2024-02-13 13:48:55 +0100

    Feedback on validation error

--- resources/views/groceries/create.blade.php
+++ resources/views/groceries/create.blade.php
@@ -9,6 +9,16 @@
                 <input style="margin-left: 24px;" type="number" name="price" step=".01"><br><br>
                 <label>Amount:</label>
                 <input type="number" name="amount"><br>
+                @if ($errors->any())
+                <div class="alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <label>{{ $error }}</label>
+                        <br>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <button class=create-button type="submit">Create</button>
         </form>
--- resources/views/groceries/edit.blade.php
+++ resources/views/groceries/edit.blade.php
@@ -10,6 +10,16 @@
                 <input style="margin-left: 24px;" type="number" name="price" step=".01" value="{{ $grocery->price }}"><br><br>
                 <label>Amount:</label>
                 <input type="number" name="amount" value="{{ $grocery->amount }}"><br>
+                @if ($errors->any())
+                <div class="alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <label>{{ $error }}</label>
+                        <br>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <button class=create-button type="submit">Edit</button>
         </form>
